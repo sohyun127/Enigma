c8514e2f03d73a4dceb640d4da35c5afaaf558de
Lia <so405@naver.com>
2022-12-18 03:06:06 +0900

    에니미 순찰&추적

--- Final/Assets/Scripts/MoveAgent.cs
+++ Final/Assets/Scripts/MoveAgent.cs
@@ -12,10 +12,20 @@ public class MoveAgent : MonoBehaviour
     //NavMeshAgent agent;
     NavMeshAgent agent;
 
+    float patrolSpeed = 1.5f; //정찰 속도
+    float traceSpeed = 4.0f; //추적 속도
+
+    //순찰 여부를 판단하는 변수
+    bool patrolling;
+    //추적 대상의 위치를 저장하는 변수
+    Vector3 traceTarget;
+
     void Start()
     {
         //NavMeshAgent 컴포넌트를 추출한 후 변수에 저장
         agent = GetComponent<NavMeshAgent>();
+        agent.speed = patrolSpeed;
+
         //하이러키 뷰의 WayPointGroup 게임오브젝트를 추출
         GameObject group = GameObject.Find("WayPointGroup");
         if(group != null)
@@ -27,7 +37,7 @@ public class MoveAgent : MonoBehaviour
             wayPoints.RemoveAt(0);
         }
 
-        MoveWayPoint();
+        SetPatrolling(true);
     }
 
     //다음 목적지까지 이동 명령을 내리는 함수
@@ -43,6 +53,10 @@ public class MoveAgent : MonoBehaviour
 
     void Update()
     {
+        //순찰 모드가 아닐 경우 이후 로직을 수행하지 않음
+        if (!patrolling)
+            return;
+
         //NavMeshAgent가 이동하고 있고 목적지에 도착했는지 여부를 계산
         if(agent.velocity.magnitude>=0.2f&&agent.remainingDistance<=0.5f)
         {
@@ -51,5 +65,45 @@ public class MoveAgent : MonoBehaviour
             //다음 목적지로 이동 명령을 수행
             MoveWayPoint();
         }
+    }
+
+    public void SetPatrolling(bool patrol)
+    {
+        patrolling = patrol;
+        agent.speed = patrolSpeed;
+        agent.angularSpeed = 120;
+        MoveWayPoint();
+    }
+
+    public void SetTraceTarget(Vector3 pos)
+    {
+        traceTarget = pos;
+        agent.speed = traceSpeed;
+        agent.angularSpeed = 360;
+        TraceTarget(traceTarget);
+    }
+
+    //주인공을 추적할 때 이동시키는 함수
+    void TraceTarget(Vector3 pos)
+    {
+        if (agent.isPathStale)
+            return;
+        agent.destination = pos;
+        agent.isStopped=false;
+    }
+
+    //순찰 및 추적을 정지시키는 함수
+    public void Stop()
+    {
+        agent.isStopped=true;
+        //바로 정지하기 위해 속도를 0으로 설정
+        agent.velocity = Vector3.zero;
+        patrolling = false;
+    }
+
+    //NavMeshAgent의 이동 속도 얻기
+    public float GetSpeed()
+    {
+        return agent.velocity.magnitude;
     }
 }
